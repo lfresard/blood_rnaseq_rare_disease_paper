77965c450c430f59c84b79c6d5ac5b67d8c9c724
lfresard <lfresard@users.noreply.github.com>
2018-09-05 15:46:29 -0700

    Update launch_RD_vcf_to_RVbed.sh

--- data_processing/genotypes/annotation/launch_RD_vcf_to_RVbed.sh
+++ data_processing/genotypes/annotation/launch_RD_vcf_to_RVbed.sh
@@ -1,5 +1,3 @@
-#LF
-#Jan 2018
 
 # This script runs RD_vcf_toRVbed.sh
 # filters for Rare variants
@@ -12,4 +10,4 @@ nproc=10
 
 vcf_suffix="_homogenized_gnomad_cadd.vcf.gz"
 
-ls RD*${vcf_suffix} | parallel --jobs $nproc "bash /users/lfresard/repos/rare_disease/scripts/vcf_handling/RD_vcf_to_RVbed.sh {}"
+ls RD*${vcf_suffix} | parallel --jobs $nproc "bash RD_vcf_to_RVbed.sh {}"
