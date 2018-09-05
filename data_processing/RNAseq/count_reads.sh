5a0ae802dac500d8dcf6b338cb3d535ac1ec7a7b
lfresard <lfresard@users.noreply.github.com>
2018-09-05 15:52:18 -0700

    Delete count_reads.sh

--- data_processing/RNAseq/count_reads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script simply counts the number of reads in a gzipped FASTQ file
-
-fastq=$1
-
-linecount=$(zcat $fastq | wc -l)
-readcount=$((linecount/4))
-
-echo -e $fastq"\t"$linecount"\t"$readcount
