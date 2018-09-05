9279cc0d6970d5c3837a1255774556629b158c43
lfresard <lfresard@users.noreply.github.com>
2018-09-05 15:52:12 -0700

    Delete count_reads_in_parallel.sh

--- data_processing/RNAseq/count_reads_in_parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script counts the number of reads we have sequenced for this project
-
-find /srv/scratch/restricted/rare_diseases/data/fastq/ \
-    | grep fastq.gz \
-    | sort \
-    | parallel  "./count_reads.sh {}" \
-    > /srv/scratch/restricted/rare_diseases/analysis/fastq_analysis/raw_read_counts.txt
-
